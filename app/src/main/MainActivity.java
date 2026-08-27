ea1d6029aad47f81ec7b952230738cce9e55c683
talentdavid787898-cell <talentdavid787898@gmail.com>
2026-08-27 16:05:21 +0100

    Update MainActivity to use activity_main layout

--- app/src/main/MainActivity.java
+++ app/src/main/MainActivity.java
@@ -2,18 +2,12 @@ package com.expertoptionsignal.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView textView = new TextView(this);
-        textView.setText("Expert Option Signal App");
-        textView.setTextSize(24);
-
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
     }
 }
